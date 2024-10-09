aed98606c970156748b86394bd67bd1b6892f900
Quentin-Epitech <quentin.bonnet@epitech.digital>
2024-10-09 15:02:29 +0200

    exo2

---
+++ ex_02/ex_02.js
@@ -0,0 +1,27 @@
+var map = L.map('map').setView([46.603354, 1.888334], 6);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+}).addTo(map);
+
+
+var epitechIcon = L.icon({
+  iconUrl: 'https://www.pngkit.com/png/detail/459-4596239_epitech-epitech-logo-transparent.png',
+  iconSize: [50, 50], 
+  iconAnchor: [25, 50], 
+  popupAnchor: [0, -50]  
+});
+
+var schools = [
+  { name: "Paris", coords: [48.9416, 2.3653] },
+  { name: "Nantes", coords: [47.1856, -1.5983] },
+  { name: "Bordeaux", coords: [44.7745, -0.5753] },
+  { name: "Lyon", coords: [45.0835, 4.8097] },
+  { name: "Toulouse", coords: [43.2876, 1.4623] }
+];
+
+schools.forEach(function(school) {
+  L.marker(school.coords, { icon: epitechIcon })
+    .addTo(map)
+    marker.bindPopup("<b>" + school.name + " rocks!</b>");
+});
